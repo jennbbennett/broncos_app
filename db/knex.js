c71713d479752ed49f51027650faf609bafef389
Jennifer Bennett <jennbbennett@hotmail.com>
2016-01-05 08:54:32 -0700

    changed process.env.ENVIRONMENT

--- db/knex.js
+++ db/knex.js
@@ -1,3 +1,3 @@
-var environment = process.env.NODE_ENV || "development";
+var environment = process.env.ENVIRONMENT || "development";
 var config = require('./../knexfile.js')[environment];
 module.exports = require('knex')(config);
